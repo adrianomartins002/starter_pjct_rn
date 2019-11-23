2c883e3b87668ba9334c05041a3d3f7ee27dabb2
Adriano <adrianomartins002@gmail.com>
2019-11-23 20:27:09 -0300

    feat(criar): criar projeto

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <View>
+        <Text>Criação do projeto</Text>
+      </View>
+    </>
+  );
+};
+
+export default App;
